c1b015b51d1dc544f0f2d11217351760cce297a5
qrice28 <118188288+qrice28@users.noreply.github.com>
2023-02-06 19:11:11 +0100

    compiles

--- FIFA OCR.cpp
+++ FIFA OCR.cpp
@@ -1,33 +1,44 @@
 #include <iostream>
-#include <stdio.h>
-#include <fcntl.h>
-#include <io.h>
+#include <locale>
+#include <string>
+#include <clocale>
 #include <aspose_ocr.h>
 #include <Aspose.Cells.h>
 
-void PerformOcrOnImage() {
-    
-    std::string image_path = "E:\\c++\\FIFA OCR\\data\\sample.png";
+string folder_path = "E:\\c++\\FIFA OCR\\data";
+
+
+String PerformOcrOnImage() {
+        
+    std::string image_path = folder_path + "\\sample.png";
     const size_t len = 4096;
     wchar_t buffer[len] = { 0 };
     size_t size = aspose::ocr::page(image_path.c_str(), buffer, len);
     std::wcout << buffer;
+    return buffer;
 }
 
-void AddingWorksheetsToNewExcelFile()
+
+void PrintToExcel(String textResult)
 {
-    StringPtr outDir = new String("E:\\c++\\FIFA OCR\\data\\");
+    String folderPath = folder_path.c_str();
+    StringPtr outDir = new String(folderPath);
     StringPtr outputManageWorksheets = outDir->StringAppend(new String("result.xlsx"));
     intrusive_ptr<IWorkbook>  workbook = Factory::CreateIWorkbook();
     int i = workbook->GetIWorksheets()->Add();
     intrusive_ptr<IWorksheet> worksheet = workbook->GetIWorksheets()->GetObjectByIndex(i);
+    intrusive_ptr<ICells> cells = worksheet->GetICells();
+    intrusive_ptr<ICell> cell = cells->GetObjectByIndex(new String("A1"));
+    Console::Write(new String("HAHA"));
+    Console::WriteLine(cell->GetStringValue());
     worksheet->SetName(new String("My Worksheet"));
     workbook->Save(outputManageWorksheets);
 }
 
 int main()
 {
-    PerformOcrOnImage();
-    AddingWorksheetsToNewExcelFile();
+    String textResult = PerformOcrOnImage();
+    PrintToExcel(textResult);
+    
     return 0;
 }
